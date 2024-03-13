29dd657a5b7c0c8fe17ad0b68303789ba4ea06b9
Naoki Yasuraoka <yasuraok@gmail.com>
2024-03-13 11:41:17 +0900

    Change model

--- extension.js
+++ extension.js
@@ -6,10 +6,25 @@ const EXT_NAME = "simple-text-refine-with-gpt"
 
 const DEFAULT_MODEL = "gpt-3.5-turbo"
 
+const MODELS = [
+    { label: DEFAULT_MODEL, description: "" },
+    { label: "gpt-4-turbo-preview", description: "" },
+]
+
 // vscode APIの設定を取得する
 function getConfigValue(key) {
     const config = vscode.workspace.getConfiguration(EXT_NAME)
     return config.get(key)
+}
+
+async function changeModel() {
+    const result = await vscode.window.showQuickPick(MODELS);
+    if (result) {
+        // vscodeの設定にmodelを保存する
+        const config = vscode.workspace.getConfiguration(EXT_NAME)
+        config.update('model', result.label, vscode.ConfigurationTarget.Global)
+        vscode.window.showInformationMessage(`Changed model to ${result.label}`)
+    }
 }
 
 async function callGPT(text, uri){
@@ -41,6 +56,12 @@ function activate(context) {
         `${EXT_NAME}.helloWorld`, () => {
             vscode.window.showInformationMessage('Hello, world!')
         }
+    ))
+
+    // モデルを変更する
+    context.subscriptions.push(vscode.commands.registerCommand(
+        `${EXT_NAME}.changeModel`,
+        changeModel
     ))
 
     // ファイル全体をGPTに添削させる
